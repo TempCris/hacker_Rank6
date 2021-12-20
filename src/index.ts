73f98259c6cece3a1340088d082b1471a5bb4a7e
kross <cristemperatum@gmail.com>
2021-12-20 17:07:06 -0600

    ejercicio 6

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+// Source: https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function miniMaxSum(arr: number[]): void {
+  const longitud = arr.length
+  let sumaMin = 0;
+  let sumaMax = 0;
+  const order = arr.sort((a, b) => a - b);
+
+  for (let i = 1; i < longitud; i++) {
+    sumaMax += order[i];
+  }
+  for (let i = 0; i < longitud - 1; i++) {
+    sumaMin += order[i];
+  }
+  console.log(sumaMin, sumaMax);
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const testArray = [1, 3, 5, 7, 9];
+  const result = miniMaxSum(testArray);
   console.log('Result: ', result);
 }
 
